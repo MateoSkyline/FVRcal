d652df9503de25e3855e9bae4b7ad840d605de41
MateoSkyline <m.skyline98@gmail.com>
2020-10-10 21:35:45 +0200

    v0.0.1 - Sucessful connection to database

---
+++ Migrations/20201010185630_Add storage type.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace FVRcal.Migrations
+{
+    public partial class Addstoragetype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Storage_Type",
+                columns: table => new
+                {
+                    st_type_id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    type = table.Column<string>(maxLength: 12, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Storage_Type", x => x.st_type_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Storage_Type");
+        }
+    }
+}
