49615e0e6107e2a5a1a1e33c1d502cad78d06dc5
MateoSkyline <m.skyline98@gmail.com>
2020-10-10 21:35:45 +0200

    v0.0.1 - Sucessful connection to database

---
+++ Migrations/20201010174229_Launch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace FVRcal.Migrations
+{
+    public partial class Launch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    firstname = table.Column<string>(maxLength: 60, nullable: true),
+                    lastname = table.Column<string>(maxLength: 60, nullable: true),
+                    username = table.Column<string>(maxLength: 30, nullable: true),
+                    usercode = table.Column<string>(maxLength: 6, nullable: true),
+                    password = table.Column<string>(maxLength: 128, nullable: true),
+                    salt = table.Column<string>(maxLength: 16, nullable: true),
+                    permissions = table.Column<string>(maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.user_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Storage",
+                columns: table => new
+                {
+                    storage_id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    user_id = table.Column<int>(nullable: false),
+                    type = table.Column<int>(nullable: false),
+                    time = table.Column<DateTime>(nullable: false),
+                    flags = table.Column<string>(maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Storage", x => x.storage_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Storage");
+        }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.EntityFrameworkCore;
+using MySQL.Data.EntityFrameworkCore;
+
+namespace FVRcal.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Storage> Storage { get; set; }
+        public DbSet<Storage_Type> Storage_Type { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseMySQL("server=192.168.0.80;port=3306;database=fvrcal;user=root;password=SeksSylcia01012020;");
+    }
+
+    public class Account
+    {
+        [Key]
+        public int user_id { get; set; }
+        [StringLength(60)]
+        public string firstname { get; set; }
+        [StringLength(60)]
+        public string lastname { get; set; }
+        [StringLength(30)]
+        public string username { get; set; }
+        [StringLength(6)]
+        public string usercode { get; set; }
+        [StringLength(128)]
+        public string password { get; set; }
+        [StringLength(16)]
+        public string salt { get; set; }
+        [StringLength(256)]
+        public string permissions { get; set; }
+    }
+
+    public class Storage
+    {
+        [Key]
+        public int storage_id { get; set; }
+        public int user_id { get; set; }
+        public int type { get; set; }
+        public DateTime time { get; set; }
+        [StringLength(64)]
+        public string flags { get; set; }
+    }
+
+    public class Storage_Type
+    {
+        [Key]
+        public int st_type_id { get; set; }
+        [StringLength(12)]
+        public string type { get; set; }
+    }
+}
