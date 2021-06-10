17a419cba935829f33757c6c57543f4b2452f128
MateoSkyline <m.skyline98@gmail.com>
2021-06-10 14:19:07 +0200

    feat(teams): added teams and teamassignments models

---
+++ Migrations/20210610121056_Add team models.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace FVRcal.Migrations
+{
+    public partial class Addteammodels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TeamAssignments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Team_Id = table.Column<int>(nullable: false),
+                    User_Id = table.Column<int>(nullable: false),
+                    Share = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamAssignments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 60, nullable: true),
+                    Description = table.Column<string>(maxLength: 2000, nullable: true),
+                    Visibility = table.Column<int>(nullable: false),
+                    Creator = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TeamAssignments");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -18,6 +18,8 @@ namespace FVRcal.Models
         {
         }
 
+        public DbSet<Teams> Teams { get; set; }
+        public DbSet<TeamAssignments> TeamAssignments { get; set; }
     }
 
     public class Account
@@ -36,34 +38,24 @@ namespace FVRcal.Models
         public string Password { get; set; }
     }
 
-    //public class Teams
-    //{
-    //    public int Id { get; set; }
-    //    [StringLength(60)]
-    //    public string Name { get; set; }
-    //    [StringLength(2000)]
-    //    public string Description { get; set; }
-    //    public int Type_Id { get; set; }
-    //    public int Status_Id { get; set; }
-    //    public int Visibility { get; set; }
-    //    public decimal Commission { get; set; }
-    //}
+    public class Teams
+    {
+        public int Id { get; set; }
+        [StringLength(60)]
+        public string Name { get; set; }
+        [StringLength(2000)]
+        public string Description { get; set; }
+        public int Visibility { get; set; }
+        public int Creator { get; set; }
+    }
 
-    //public class TeamRoles
-    //{
-    //    public int Id { get; set; }
-    //    [StringLength(60)]
-    //    public string Name { get; set; }
-    //}
-
-    //public class TeamAssignments
-    //{
-    //    public int Id { get; set; }
-    //    public int Team_Id { get; set; }
-    //    public int User_Id { get; set; }
-    //    public int Role_Id { get; set; }
-    //    public decimal Share { get; set; }
-    //}
+    public class TeamAssignments
+    {
+        public int Id { get; set; }
+        public int Team_Id { get; set; }
+        public int User_Id { get; set; }
+        public decimal Share { get; set; }
+    }
 
     //public class Orders
     //{
